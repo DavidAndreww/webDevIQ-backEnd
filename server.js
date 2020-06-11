ab5eea2158252a11a7ff57b3f8afe7c9cbc60214
David Bille <d_bille@yahoo.com>
2020-06-10 20:53:11 -0500

    server is set up. installed nodemon and colors devDependencies, express and cors dependencies

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('server')
+const express = require('express')
+const colors = require('colors')
+
+const app = express()
+const port = 3030;
+
+
+app.get('/', (req, res) =>{
+  res.send('fresh data here, get it while it\'s hot')
+})
+
+app.listen(port, ()=>{
+  console.log(`App is running on port ${port}`.blue.bold)
+})
