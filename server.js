788eb32f69d9936adfdd1faffa4b84549e2151ad
David Bille <d_bille@yahoo.com>
2020-06-10 21:42:18 -0500

    enabling cors to work through our server only.

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const cors = require("cors");
 const colors = require("colors");
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("fresh data here, get it while it's hot!!");
+app.use(express.json());
+// app.use(cors())
+
+let corsOptions = {
+  origin: 'http://localhost:3000', // whatever our domain is
+  optionsSuccessStatus: 200
+}
+
+app.get("/", cors(corsOptions), (req, res) => {
+  res.send("fresh data here, get it while it's hot!");
 });
 
 app.listen(PORT, () => {
