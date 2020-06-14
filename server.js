c6334fec3fd2ea40ddf2e2bf1a91be020e74a718
unknown <d_bille@yahoo.com>
2020-06-14 11:48:03 -0500

    more templates loaded to server.js from auth0 setup site

--- server.js
+++ server.js
@@ -4,7 +4,56 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const colors = require("colors");
 const PORT = process.env.PORT || 3030;
+// used for auth0
+const session = require('express-session')
+// load passport
+const passport = require('passport')
+const Auth0Strategy = require('passport-auth0')
 
+// configuring express-session
+let sess = {
+  secret: 'helloGoodbye',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+}
+
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
+
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  // or if you're encountering the error message:
+  // "Unable to verify authorization request state"
+  // app.set('trust proxy', 1);
+}
+
+app.use(session(sess));
+
+// configure passport to use Auth0
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// routes for users and questions
 const userRouter = require('./routes/userRouter')
 const questionsRouter = require('./routes/questionsRouter')
 
