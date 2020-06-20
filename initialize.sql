75610ee2b7e123d7b813b89d8915474572e84674
unknown <d_bille@yahoo.com>
2020-06-19 20:28:30 -0500

    added questions data into questions table

--- initialize.sql
+++ initialize.sql
@@ -22,6 +22,7 @@ CREATE TABLE questions (
 );
 
 INSERT INTO questions
-    (id, genre, question, options, answer, resources)
+    (id, genre, question, answer, options, resources)
 VALUES
-(1, 'JavaScript', 'What is the result of: 5 + 5 + "7"?', '17, 10, 557, 107', '107', 'https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Math, https://www.youtube.com/watch?v=9Q8BAZffbz8'),
+(1, 'JavaScript', 'What is the result of: 5 + 5 + "7"?', '107', '17, 10, 557, 107', 'https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Math, https://www.youtube.com/watch?v=9Q8BAZffbz8'),
+(2, 'JavaScript', 'Explain parseInt()', 'String method that returns all integers in the string', 'Number method that checks number and returns a boolean, Number method that combines two integers, String method that returns all integers in the string, Math method that turns an integer into a string', 'https://www.bitdegree.org/learn/javascript-parseint, https://www.youtube.com/watch?v=OXpTBQtL4nY');
