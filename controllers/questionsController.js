a0f849d28d1450cc51db1af8c068c03cab7e0a24
unknown <d_bille@yahoo.com>
2020-06-20 11:52:06 -0500

    added more questions to initialize.sql file

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -2,7 +2,7 @@ const { data } = require('../dummyData')
 const pool = require('../sql/connection')
 
 const getQuestions = (req, res) => {
-    res.json(data)
+    
 }
 
 module.exports = { getQuestions }
