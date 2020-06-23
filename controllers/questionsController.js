107960b797387d5d69d44b7729e4ca2a3f6fa1d4
unknown <d_bille@yahoo.com>
2020-06-22 20:50:05 -0500

    finished finding correct sql syntax to allow for us to search questions and return multiple genres!

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -5,17 +5,17 @@ const sqlErrorHandler = require('../sql/errorHandler');
 
 const getQuestions = (req, res) => {
     let { questions } = req.body;
-    let queryLimit = Math.round(Math.ceil(10/questions.length))
-    console.log(limit)
-    res.json({questions})
-    
+    console.log(questions)
+    let queryLimit = Math.round(Math.ceil(10 / questions.length))
+    console.log(queryLimit)
 
-    // pool.query(
-    //     `SELECT * FROM questions WHERE genre=`` LIMIT ${queryLimit}`, (err, results) => {
-    //         if (err) return sqlErrorHandler(res, err)
-    //         res.json(results)
-    //     }
-    // )
+    pool.query(
+        `SELECT * FROM questions WHERE genre IN ("${questions[0]}", "${questions[1]}") GROUP BY id HAVING COUNT(2) LIMIT ${queryLimit} `, (err, results) => {
+            if (err) return sqlErrorHandler(res, err)
+            res.send(results)
+        }
+    )
+
 }
 
 module.exports = { getQuestions }
