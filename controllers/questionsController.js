18b8184c3ccc615384e2f318806dcc83fc4b6c98
unknown <d_bille@yahoo.com>
2020-06-22 21:47:12 -0500

    ok I lied. query statement works, but need to limit total results to 10 while still pulling RANDOM questions from each genre. also created function trimResults to trim off extra questions if sent back from db

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -2,17 +2,40 @@ const { data } = require('../dummyData')
 const pool = require('../sql/connection')
 const { sqlErrorhandler } = require('../sql/errorHandler');
 const sqlErrorHandler = require('../sql/errorHandler');
+const { query } = require('express');
+
+function getSQLStatement(questions) {
+    let syntax;
+    switch (questions.length) {
+        case 1:
+            syntax = `SELECT * FROM questions WHERE genre IN ("${questions[0]}") GROUP BY id HAVING COUNT(${questions.length}) ORDER BY genre`
+            break;
+        case 2:
+            syntax = `SELECT * FROM questions WHERE genre IN ("${questions[0]}", "${questions[1]}") GROUP BY id HAVING COUNT(${questions.length}) ORDER BY genre`
+            break;
+        case 3:
+            syntax = `SELECT * FROM questions WHERE genre IN ("${questions[0]}", "${questions[1]}", "${questions[2]}") GROUP BY id HAVING COUNT(${questions.length}) ORDER BY genre`
+            break;
+        case 4:
+            syntax = `SELECT * FROM questions WHERE genre IN ("${questions[0]}", "${questions[1]}", "${questions[2]}", "${questions[3]}") GROUP BY id HAVING COUNT(${questions.length}) ORDER BY genre`
+            break;
+        default:
+            ''
+    }
+    return syntax
+}
+function trimResults(results){
+    if (results.length > 10) results.splice(10)
+    return results
+}
 
 const getQuestions = (req, res) => {
     let { questions } = req.body;
-    console.log(questions)
     let queryLimit = Math.round(Math.ceil(10 / questions.length))
-    console.log(queryLimit)
-
     pool.query(
-        `SELECT * FROM questions WHERE genre IN ("${questions[0]}", "${questions[1]}") GROUP BY id HAVING COUNT(2) LIMIT ${queryLimit} `, (err, results) => {
+        getSQLStatement(questions), (err, results) => {
             if (err) return sqlErrorHandler(res, err)
-            res.send(results)
+            res.send(trimResults(results))
         }
     )
 
