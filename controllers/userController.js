23c3bc0dfe20d1ac186d03a23ae4e5501a2d129a
unknown <d_bille@yahoo.com>
2020-08-01 20:34:50 -0500

    set up route and controller to update user questions attempted anc correctly answered

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,33 @@
+const pool = require("../sql/connection");
+// SQL error handler
+const { sqlErrorhandler } = require("../sql/errorHandler");
+
 const login = (req, res) => {
-    const { id, name, email } = req.body;
-    console.log(id)
-    console.log(user)
-    res.json({loggedIn: user})
-}
+  const { id, name, email } = req.body;
+  console.log(id);
+  console.log(user);
+  res.json({ loggedIn: user });
+};
 
 const signup = (req, res) => {
-    const { name, email } = req.body;
-    res.json({ newUser: { name: name, email: email } })
-}
+  const { name, email } = req.body;
+  res.json({ newUser: { name: name, email: email } });
+};
 
-module.exports = { login, signup }
+const updateAttempts = (req, res) => {
+  const { totalAttempts, correctAttempts, id } = req.body;
+
+  let updatedRows = {
+    correctAttempts: correctAttempts,
+    totalAttempts: totalAttempts,
+  };
+  pool.query(
+    `UPDATE users SET totalAttempts = ${totalAttempts}, correctAttempts = ${correctAttempts} where id = ${id}`,
+    (err, results) => {
+      if (err) return sqlErrorHandler(res, err);
+      res.send(updatedRows);
+    }
+  );
+};
+
+module.exports = { login, signup, updateAttempts };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { login, signup } = require('../controllers/userController')
+const { login, signup, updateAttempts } = require('../controllers/userController')
 
 router.post('/', login)
 router.post('/signup', signup)
+router.put('/update', updateAttempts)
 
 module.exports = router;
